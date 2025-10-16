448fba6f6d6701b4c494da4e4ebb9d87d0e5f0bf
VeenaVinayan <veenavinayan@gmail.com>
2025-10-16 18:07:57 +0530

    Initial Commit

---
+++ src/components/todo.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import { Delete , Edit ,SaveIcon } from 'lucide-react';
+
+type TTodo= {
+    todo:string;
+    id:number;
+    status:boolean;
+}
+
+const Todo : React.FC = () => {
+  const [ todo, setTodo ] = useState<string>("");  
+  const [todos, setTodos ] = useState<TTodo[]>([]);
+  const [count,setCount] = useState<number>(0);
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [edit,setEdit] = useState<string>('');
+  const [editId, setEditId ] = useState<number>();
+  const [ empty ,setIsEmpty ] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setIsEmpty(false);
+    setTodo(value);
+  };
+  
+  const handleTodos = () =>{
+     setCount(count+1);
+     if(todo === ""){
+         setIsEmpty(true);
+         return;
+     }
+     setTodos((prev) => ([ ...prev, { id: count, todo: todo ,status:false} ]));
+     setTodo("");
+  }
+  const handleEdit = (id:number) =>{
+      setIsEdit(true);
+      setEditId(id);
+  }
+  const handleDelete = ( id :number) =>{
+     setTodos((prev) => (prev.filter( todo => todo.id !== id)) )
+  }
+  const handleCancel = () => {
+      setIsEdit(false);
+  }
+  const handleSave = (id: number) => {
+        setTodos(prev => prev.map((item: TTodo) => item.id === id ? { ...item, todo: edit } : item));
+        setIsEdit(false);
+        setEdit('');
+  }
+  const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+     setEdit(e.target.value)
+  }
+  const handleStatus = (id: number) =>{
+     setTodos(prev => prev.map((item :TTodo) => item.id === id ? {...item,status:true } : item))
+  }
+  return (
+    <>
+      <div className='bg-black flex  flex-col justify-center items-center rounded m-2 p-10 min-h-screen'>
+        <h2 className='text-amber-800  font-extrabold text-4xl shadow'>ToDo App</h2>
+        <div className='items-center m-3 p-5 flex flex-row'>
+           <div className='flex flex-col m-1 p-2'>
+            <input type="text" value={todo} onChange={handleChange} className='bg-amber-50 text-black rounded border-amber-800 w-44'  />
+            { empty && (<span className='text-red-400'>Add todo properly</span> )}
+            </div>    
+          <button onClick={ handleTodos } className='border-amber-700 p-2 m-2 text-white font-medium border rounded'>Add</button>
+        </div>
+        {
+            todos.length>0 && (
+                <div className=' border rounded border-amber-900 w-72 justify-around'>
+                      { todos.map((todo :TTodo,index : number) => (
+                          <div className='flex flex-row p-2 items-center' key={todo.id}>
+                             { isEdit && editId === todo.id  ? (
+                                 <>
+                                     <input value={edit || todo.todo}  onChange={(e) =>handleEditChange(e)} className='text-white p-1 m-1 border rounded' />
+                                     <SaveIcon size={18} color={'white'} onClick={() =>handleSave(todo.id)} />
+                                     <button className='text-white' onClick={handleCancel}>Cancel</button>
+                                 </> 
+                               ):( 
+                                <>
+                                    <span className='text-white '>{index +1 }</span>
+                                    <input type="checkbox" onClick={() => handleStatus(todo.id)}/>
+                                    <p className='p-2 text-white'>{todo.todo}</p>
+                                    <Edit size={20} color={'white'} onClick={() => handleEdit(todo.id)}/>
+                                    <Delete size={20} color={'white'} onClick={() =>handleDelete(todo.id)} />
+                                </>
+                            )}  
+                          </div>
+                      ))}
+                </div>    
+            )
+        }
+     </div>   
+    </>
+  )
+}
+
+export default Todo
