00d81d33f6657287b5dade119ec8550a7689b4cb
VeenaVinayan <veenavinayan@gmail.com>
2025-10-17 15:13:53 +0530

    commit after modify

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -58,7 +58,7 @@ const Todo : React.FC = () => {
         <h2 className='text-amber-800  font-extrabold text-4xl shadow'>ToDo App</h2>
         <div className='items-center m-3 p-5 flex flex-row'>
            <div className='flex flex-col m-1 p-2'>
-            <input type="text" value={todo} onChange={handleChange} className='bg-amber-50 text-black rounded border-amber-800 w-44'  />
+            <input type="text" value={todo} onChange={handleChange} className='bg-amber-50 text-black rounded border-amber-800 w-72 h-12 text-2xl'  />
             { empty && (<span className='text-red-400'>Add todo properly</span> )}
             </div>    
           <button onClick={ handleTodos } className='border-amber-700 p-2 m-2 text-white font-medium border rounded'>Add</button>
@@ -67,11 +67,9 @@ const Todo : React.FC = () => {
             todos.length>0 && (
                 <>
                 <div className='flex flex-row'> 
-                 <div className=' border rounded border-amber-900 w-72 justify-around'>
-                    <h2 className='text-2xl font-extralight text-amber-700'>Pending Tasks</h2>
+                 <div className='  w-72 justify-around'>
                       { todos.map((todo :TTodo) => (
-                         !todo.status && ( 
-                          <div className='flex flex-row p-2 items-center' key={todo.id}>
+                         <div className='flex flex-row p-2 m-2 items-center border rounded border-amber-900 justify-around' key={todo.id}>
                              { isEdit && editId === todo.id  ? (
                                  <>
                                      <input value={edit || todo.todo}  onChange={(e) =>handleEditChange(e)} className='text-white p-1 m-1 border rounded' />
@@ -87,18 +85,10 @@ const Todo : React.FC = () => {
                                 </>
                             )}  
                           </div>
-                        ))
+                      )
                     )}
                 </div>    
-                <div className='border rounded border-amber-800 m-3 p-3'>
-                   <h2 className='font-extralight text-2xl text-amber-800'>Completed Task</h2>
-                    { todos.map((item : TTodo) =>  (  
-                        item.status && ( 
-                         <div>
-                             <p className='text-white'>{item.todo}</p>
-                         </div>   
-                    )))}
-                </div>  
+                
                </div>   
               </>
             )
